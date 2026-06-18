1573ff3633574978a139575ae09e67d23b97582e
webcuser <97110714+webcuser@users.noreply.github.com>
2026-06-18 15:40:10 +0200

    feat: add frontend/src/App.vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <h1>Habit Tracker</h1>
+    <ul>
+      <li v-for="habit in habits" :key="habit.id">
+        {{ habit.name }} - Completed: {{ habit.completed }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      habits: []
+    };
+  },
+  mounted() {
+    this.fetchHabits();
+  },
+  methods: {
+    async fetchHabits() {
+      const response = await axios.get('http://localhost:8000/habits/');
+      this.habits = response.data;
+    }
+  }
+};
+</script>
